2df106e4b1056c1253c6c89e772d7a4d84ec6897
michelle-lo <mlo20@stuy.edu>
2020-12-13 20:43:31 -0500

    Revised some formatting

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -6,7 +6,6 @@ public class NoNullArrayList<T> extends ArrayList<T> {
   }
 
   public NoNullArrayList (int startingCapacity) {
-
     super(startingCapacity);
   }
 
@@ -34,12 +33,3 @@ public class NoNullArrayList<T> extends ArrayList<T> {
   }
 
 }
-
-
-/*
-1. Override set and both add methods
-a) Check the method signatures of add() add(index) and set(index,value) so you override correctly. The return types are important!
-b) They should all throw an IllegalArgumentException when null is added, otherwise call the old add method.
-2. Write 2 constructors:
-default + startingCapacity.
-*/

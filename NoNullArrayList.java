8eff49fad386618599aa01b36f9450d2efd98382
michelle-lo <mlo20@stuy.edu>
2020-12-13 20:40:05 -0500

    add + set methods all throw an exception when null is added

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -12,18 +12,23 @@ public class NoNullArrayList<T> extends ArrayList<T> {
 
   public boolean add(T element) {
     if (element == null) {
-      //throw new IllegalArgumentException("You cannot add a null");
-      return false;
+      throw new IllegalArgumentException("You cannot add a null");
     }
     super.add(element);
     return true;
   }
 
   public void add(int index, T element) {
+    if (element == null) {
+      throw new IllegalArgumentException("You cannot add a null");
+    }
     super.add(index, element);
   }
 
   public T set (int index, T element) {
+    if (element == null) {
+      throw new IllegalArgumentException("You cannot add a null");
+    }
     super.set(index, element);
     return element;
   }
--- Tester.java
+++ Tester.java
@@ -1,13 +1,37 @@
+import java.util.*;
 public class Tester {
   public static void main (String[] args) {
     NoNullArrayList<String> ah = new NoNullArrayList<String>();
     System.out.println("\nNoNullArrayList\n");
-    head("add, add(index, e)");
+    head("add, add(index, e), set");
     print(ah.add("hello"), true);
     ah.add(0, "no");
     print("" + ah, "[no, hello]");
     print(ah.set(1, "yes"), "yes");
     print("" + ah, "[no, yes]");
+
+    head("add, add(index, e), set -- with nulls");
+    try {
+      ah.add(2, null);
+    } catch (IllegalArgumentException e) {
+      System.out.println("error! -- null added");
+    }
+    try {
+      ah.add(null);
+    } catch (IllegalArgumentException e) {
+      System.out.println("error! -- null added");
+    }
+    try {
+      ah.set(2, null);
+    } catch (IllegalArgumentException e) {
+      System.out.println("error! -- null added");
+    }
+    print("" + ah, "[no, yes]");
+
+    
+
+
+    System.out.println("all good!");
 
   }
 
